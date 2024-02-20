6505e5c00159459b11d95018ac6c25af6eada544
Connor Cisney <ccisney@student.neumont.edu>
2024-02-19 19:29:42 -0700

    Added multiple buttons and some orange.

--- lib/main.dart
+++ lib/main.dart
@@ -32,22 +32,42 @@ class ListViewWidget extends StatelessWidget {
     return ListView(
       children: <Widget>[
         ListTile(
-          title: Text('Welcome to ChefQuest!'),
-          style: TextStyle (
-            fontSize: 24.0,
-            fontWeight: fontWeight.bold,
-          )
+          title: Container(
+            color: Colors.orange,
+            child: Text(
+              'Welcome to ChefQuest!',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          subtitle: Text(
+            'Organize and find your recipes here on ChefQuest!',
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
         ),
-        subtitle: Text(
-          'Organize and find your recipes here on ChefQuest!',
-          style: TextStyle(
-            fontSize: 18.0,
-          )
-        )
         ListTile(
           title: Text('Search'),
           onTap: () {
-            
+          },
+        ),
+        ListTile(
+          title: Text('Favorites'),
+          onTap: () {
+          },
+        ),
+        ListTile(
+          title: Text('Recipes'),
+          onTap: () {
+          },
+        ),
+        ListTile(
+          title: Text('Share'),
+          onTap: () {
           },
         ),
         // Add more ListTiles for additional features as needed
